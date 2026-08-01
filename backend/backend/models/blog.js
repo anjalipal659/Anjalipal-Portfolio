475a32de73d9609851558fd174aa24b2b1907633
Anjali Pal <palanjali659@gmail.com>
2026-08-01 21:01:04 +0530

    Create backend database models

---
+++ backend/backend/models/blog.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+{
+    title: {
+        type: String,
+        required: true
+    },
+
+    content: {
+        type: String,
+        required: true
+    },
+
+    image: {
+        type: String
+    },
+
+    tags: [
+        {
+            type: String
+        }
+    ]
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
---
+++ backend/backend/models/project.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+{
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    technologies: [
+        {
+            type: String
+        }
+    ],
+
+    github: {
+        type: String
+    },
+
+    liveDemo: {
+        type: String
+    },
+
+    image: {
+        type: String
+    }
+},
+{
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Project", projectSchema);
