1b487f23756a2bde92998c94c67a7bd30d3c65c5
Anjali Pal <palanjali659@gmail.com>
2026-08-02 17:17:44 +0530

    Initialize backend with Express, Mongoose and API structure

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -19,6 +19,8 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const contactRoutes = require("./routes/contactRoutes");
+app.use("/api/contact", contactRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
